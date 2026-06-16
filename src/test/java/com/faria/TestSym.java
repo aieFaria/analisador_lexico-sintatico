f1de85c10f54afa4b93b01e108f06d378f14caf3
aiefaria <gabribga2002@gmail.com>
2026-06-16 14:09:24 -0300

    nada

---
+++ src/test/java/com/faria/TestSym.java
@@ -0,0 +1,34 @@
+package com.faria;
+
+import com.faria.Sym;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
+
+public class TestSym {
+    /**
+	 * Observações: [1] Os testes do métodos não implementados estão presentes porém comentados
+	 * 				[2] o método toString() não esta formatado conforme estabelecido. Regras:
+	 * 				    a. "right <value>" ao invés de "right <value> "
+	 * 					b. "(left: <value>) right: <value>" ao invés de "(left: <value>)right: <value>"
+	 */
+	
+    @Nested
+	@DisplayName("Testando tabela de Simbolos, classe Sym")
+    public class açoesBasicas {
+
+		@Test
+		@DisplayName("Manipulando Sym de exemplo")
+		void manipulingSym() {
+			
+		}
+    
+		@Test
+		@DisplayName("Criação de uma nova arvore a partir de um array com sucesso")
+		void criandoUmaNovaArvore() {
+			
+		}
+    }
+}
